a8cf6d8720cf5727c853c52e8e4d78bfd4a5a95a
noprom <tyee.noprom@qq.com>
2015-06-10 14:04:38 +0800

    val sum = (x: Int, y: Int) => x + y
    val sum = (x: Int, y: Int) => x + y

--- src/com/huntdreams/scala/HelloScala.scala
+++ src/com/huntdreams/scala/HelloScala.scala
@@ -17,9 +17,20 @@ object HelloScala {
     println("Hello scala,this is Allen Wang!");
   }
 
+  def add = (x: Int, y: Int) => x + y
+
+  // 常量用val表示
+  val sum = (x: Int, y: Int) => x + y
+
   def main(args: Array[String]) {
     //    println("Hello Scala!");
     //    println(hello("Scala"));
-    helloscala();
+    //    helloscala();
+
+    //    helloscala
+
+    //    println(add(1, 2));
+
+    println(sum(1, 2));
   }
 }
